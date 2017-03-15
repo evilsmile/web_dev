b458ab8e473fff5bb54a6e63184d43cc6940b0db
evilsmile <freedominmind@163.com>
2017-03-15 17:04:51 +0800

    add timer/window/screen/location/history test

--- js/learn.js
+++ js/learn.js
@@ -100,13 +100,44 @@ function mOut(obj)
     obj.innerHTML = "Check mouse out";
 }
 
+function getCookie(c_name)
+{
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
+
+function setCookie(c_name, value, expiredays)
+{
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + expiredays);
+    document.cookie = c_name + "=" + escape(value) + ((expiredays == null) ? "": ";expires=" + exdate.toGMTString());
+}
+
 function checkCookies()
 {
+    username = getCookie('username');
+    if (username != null && username != "") {
+        alert("Welcome again " + username + '!');
+    } else {
+        username = prompt('Please enter your name:', "");
+        if (username != null && username != "") {
+            setCookie('username', username, 365);
+        }
+    }
+    /*
     if (navigator.cookieEnabled == true) {
         alert("Cookie enabled!");
     } else {
         alert("Cookie disabled!");
     }
+    */
 }
 
 function upper_text()
@@ -154,3 +185,83 @@ function test_regexp()
 window.document.write("window size: "  + window.innerWidth + "x" + window.innerHeight + "<br/>");
 // window.screen 对象包含有关用户屏幕的信息。
 window.document.write("screen size: "  + screen.availWidth + "x" + screen.availHeight + "<br/>");
+
+// window.location 对象用于获得当前页面的地址 (URL)，并把浏览器重定向到新的页面。
+window.document.write("hostname: " + location.hostname +"<br/>");
+window.document.write("pathname: " + location.pathname +"<br/>");
+window.document.write("port: " + location.port +"<br/>");
+window.document.write("protocol: " + location.protocol +"<br/>");
+window.document.write("href: " + location.href +"<br/>");
+
+function go_baidu()
+{
+    window.location.assign("http://www.baidu.com");
+}
+
+// window.history 对象包含浏览器的历史。
+function history_back()
+{
+    window.history.back();
+}
+
+function history_forward()
+{
+    window.history.forward();
+}
+
+// window.navigator 对象包含有关访问者浏览器的信息。
+window.document.write("Browser codeName: " + window.navigator.appCodeName + "<br/>");
+window.document.write("Browser Name: " + window.navigator.appName + "<br/>");
+window.document.write("Browser Version: " + window.navigator.appVersion + "<br/>");
+window.document.write("Cookies Enabled: " + window.navigator.cookieEnabled + "<br/>");
+window.document.write("Platform: " + window.navigator.platform + "<br/>");
+window.document.write("User-agent header: " + window.navigator.userAgent + "<br/>");
+window.document.write("User-agent language: " + window.navigator.systemLanguage + "<br/>");
+
+// 警告：来自 navigator 对象的信息具有误导性，不应该被用于检测浏览器版本，这是因为：
+// navigator 数据可被浏览器使用者更改
+// 浏览器无法报告晚于浏览器发布的新操作系统
+//
+// 由于 navigator 可误导浏览器检测，使用对象检测可用来嗅探不同的浏览器。
+// 由于不同的浏览器支持不同的对象，您可以使用对象来检测浏览器。例如，由于只有 Opera 支持属性 "window.opera"，您可以据此识别出 Opera。
+if (window.opera) {
+    window.document.write("Detect opera <br/>");
+} else {
+    window.document.write("Not opera <br/>");
+}
+
+// 警告框
+// 警告框经常用于确保用户可以得到某些信息。
+// 当警告框出现后，用户需要点击确定按钮才能继续进行操作。
+// alert("alert");
+
+// 确认框用于使用户可以验证或者接受某些信息。
+// 当确认框出现后，用户需要点击确定或者取消按钮才能继续进行操作。
+// 如果用户点击确认，那么返回值为 true。如果用户点击取消，那么返回值为 false。
+// confirm("OK");
+
+// 提示框经常用于提示用户在进入页面前输入某个值。
+// 当提示框出现后，用户需要输入某个值，然后点击确认或取消按钮才能继续操纵。
+// 如果用户点击确认，那么返回值为输入的值。如果用户点击取消，那么返回值为 null。
+// prompt("Choice?", "default");
+
+
+var cnt = 0;
+var test_timer;
+function secondsCounter()
+{
+    document.getElementById("seconds_counter").innerHTML = cnt;
+    cnt = cnt + 1;
+    test_timer = setTimeout("secondsCounter()", 1000);
+}
+
+function startCounter()
+{
+    test_timer = setTimeout("secondsCounter()", 1000);
+}
+function stopCounter()
+{
+    clearTimeout(test_timer);
+}
+
+
