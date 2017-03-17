c27912397e12234fbc54990af4ba61c104b1ffcf
evilsmile <freedominmind@163.com>
2017-03-17 17:01:44 +0800

    add jquery_learn.js

---
+++ js/jquery_learn.js
@@ -0,0 +1,12 @@
+// Query 是一个 JavaScript 函数库。
+// jQuery 库包含以下特性：
+// 1. HTML 元素选取
+// 2. HTML 元素操作
+// 3. CSS 操作
+// 4. HTML 事件函数
+// 5. JavaScript 特效和动画
+// 6. HTML DOM 遍历和修改
+// 7. AJAX
+// 8. Utilities
+
+
